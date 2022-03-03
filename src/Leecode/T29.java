e56bb75a829eab8918d2b5f095144b548325f0a2
zlq <1142401783@qq.com>
2022-03-03 19:47:41 +0800

    ""

--- src/Leecode/T29.java
+++ src/Leecode/T29.java
@@ -1,5 +1,7 @@
 package Leecode;
 
+import java.util.Scanner;
+
 /**
  * @ Author     ：赵棱泉.
  * @ Date       ：Created in 22:46 2022/2/18
@@ -7,19 +9,35 @@ package Leecode;
  */
 public class T29 {
 	public static void main(String[] args) {
-		System.out.println(divide(-2147483648, 1));
+		System.out.println(divide(-2147483648,-3));
+		//Integer.MIN_VALUE	-2147483648
+		//Integer.MAX_VALUE	2147483647
+
 	}
 	public static int divide(int dividend, int divisor) {
 		if (divisor ==1) {
 			return dividend;
 		}
 		if (divisor == -1) {
-			return -dividend;
+			if (dividend == Integer.MIN_VALUE) {
+				return Integer.MAX_VALUE;
+			}
 		}
-		int a = Math.abs(dividend);
-		int b = Math.abs(divisor);
+		long a = Math.abs((long) dividend);
+		long b = Math.abs((long) divisor);
 		int res=0;
-		while (a > b) {
+//		if (dividend == Integer.MIN_VALUE) {
+//			int res2=0;
+//			while (dividend != 0) {
+//				dividend += Math.abs(divisor);
+//				res2++;
+//			}
+//			if (divisor < 0) {
+//				return res2;
+//			}
+//			return -res2;
+//		}
+		while (a >= b) {
 			a-=b;
 			res++;
 		}
